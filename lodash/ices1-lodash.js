a28c61fb33a6e991f328371da381f5c58f07159f
ices1 <iceesong@outlook.com>
2018-07-23 22:24:09 +0800

    up

--- lodash/ices1-lodash.js
+++ lodash/ices1-lodash.js
@@ -31,15 +31,16 @@ var ices1 = function() {
   }
 
   //连接数组
-  let concat = array => {
-    let len = arguments.length
+  let concat = (array, ...args) => {
+    let len = args.length
     let arr = []
+    arr.push(...array)
 
     for (let i = 0; i < len; i++) {
-      if (typeof (arguments[i]) == 'object') {
-        arr.push(...arguments[i])
+      if (typeof (args[i]) == 'object') {
+        arr.push(...args[i])
       } else {
-        arr.push(arguments[i])
+        arr.push(args[i])
       }
     }
 
@@ -47,19 +48,16 @@ var ices1 = function() {
   }
 
   //根据后面数组，移除与前 array 中相同项
-  let difference = (array, arrFit = []) => {
+  let difference = (array, ...args) => {
+    args = concat([], ...args)
     let len = array.length
-    let lenFit = arrFit.length
+    let lenFit = args.length
     var arr = []
 
     for (var i = 0; i < len; i++) {
-      if (!arrFit.includes(array[i])) {
+      if (!args.includes(array[i])) {
         arr.push(array[i])
       }
-    }
-
-    for (var i = 2; i < arguments.length; i++) {
-      arr = ices1.difference(arr, arguments[i])
     }
 
     return arr
@@ -123,7 +121,7 @@ var ices1 = function() {
 
     for (var i = 0; i < ary.length; i++) {
       if (Array.isArray(ary[i])) {
-        var tmp = ices1.flattenDeep(ary[i])
+        var tmp = flattenDeep(ary[i])
         result.push(...tmp)
       } else {
         result.push(ary[i])
@@ -154,12 +152,11 @@ var ices1 = function() {
   }
   
   // join: ary -> str
-  let join = (array, separator=',') => {
-    let s = separator
+  let join = (ary, separator=',') => {
     let res = ''
 
-    for(let i = 0; i < array.length; i++) {
-      res += array[i]
+    for(let i = 0; i < ary.length; i++) {
+      res += i < ary.length - 1 ? ary[i] + separator : ary[i]
     }
 
     return res
@@ -198,13 +195,50 @@ var ices1 = function() {
   let pullAll = (ary, rmary) => {
     for(let i = 0; i < ary.length; i++) {
       if(rmary.includes(ary[i])) {
-        rmary.splice(i, 1)
+        ary.splice(i, 1)
         i--
       }
     }
 
     return ary
   }  
+
+  // pullAt: removes all given values By index
+  let pullAt = (ary, indexs) => {
+    let res = []
+    if(Array.isArray(indexs)) {
+      res = indexs.map( x => ary[x])
+      indexs.sort((a, b) => b - a).map( x => (ary.splice(x, 1)))
+    } else {
+      res.push(ary.splice(indexs, 1))
+    }
+
+    return res
+  }
+
+  // remove: removes all elements from ary (by function)
+  let remove = (ary, func = () => 1) => ary.filter(func)
+
+  // reverse: reverse
+  let reverse = ary => ary.map((_, index) => ary[ary.length - index - 1])
+
+  // slice: slice
+  let slice = (ary, start = 0, end = ary.length) => ary.reduce((res, _, index) => 
+    {
+      if(index >= start && index < end) res.push(ary[index])
+      return res 
+    },[])
+
+  // sortedIndex: sortedIndex
+  let sortedIndex = (ary, val) => {
+    for(let i = 0; i < ary.length; i++) {
+      if(val <= ary[i]) {
+        return i
+      }
+    }
+
+    return ary.length
+  } 
 
   //without: removes all given values from array
   let without = (ary,...args) => {
@@ -247,17 +281,17 @@ var ices1 = function() {
   let initial = ary => ary.slice(0, ary.length - 1)
 
   //求 多个 ary 交集
-  let intersection = () => {
+  let intersection = (...args) => {
     let result = []
     let tmp = []
 
-    for (let i = 1; i < arguments.length; i++) {
-      tmp = tmp.concat(arguments[i])
+    for (let i = 1; i < args.length; i++) {
+      tmp = tmp.concat(args[i])
     }
 
-    for (var i = 0; i < arguments[0].length; i++) {
-      if (tmp.includes(arguments[0][i])) {
-        result.push(arguments[0][i])
+    for (var i = 0; i < args[0].length; i++) {
+      if (tmp.includes(args[0][i])) {
+        result.push(args[0][i])
       }
     }
 
@@ -268,7 +302,7 @@ var ices1 = function() {
 
   //字符串转数字
   let parseInt = str => {
-    let tmp
+    let tmp = ''
 
     for(let i = 0;i < str.length; i++) {
       if(+str[i] !== +str[i]) {
@@ -303,7 +337,13 @@ var ices1 = function() {
 
 
 
-
+  let iteratee = iter => {
+    if(typeof iter === 'function') {
+      return iter
+    } else if (Array.isArray(iter)) {
+      return 
+    }
+  }
 
 
   return {
@@ -312,24 +352,44 @@ var ices1 = function() {
     compact: compact,
     concat: concat,
     difference: difference,
+// differenceBy: differenceBy,
+// differenceWith: differenceWith,
     drop: drop,
     dropRight: dropRight,
+// dropRightWhile: dropRightWhile,
+// dropWhile: dropWhile,
     fill: fill,
+// findIndex: findIndex,
+// findLastIndex: findLastIndex,
     flatten: flatten,
     flattenDeep: flattenDeep,
     flattenDepth: flattenDepth,
+    fromPairs: fromPairs,
+    head: head,
+    indexOf: indexOf,
+    initial: initial,
+    intersection: intersection,
+    // intersectionBy: intersectionBy,
+    // intersectionWith: intersectionWith,
     join: join,
     last: last,
     lastIndexOf: lastIndexOf,
     nth: nth,
     pull: pull,
     pullAll: pullAll,
+    // pullAllBy: pullAllBY,
+    // pullAllWith: pullAllWith,
+    pullAt: pullAt,
+    remove: remove,
+    reverse: reverse,
+    slice: slice,
+    sortedIndex: sortedIndex,
+  // sortedIndexBy: sortedIndexBy,
+  // sortedIndexOf: sortedIndexOf,
+
+
+
     without: without,
-    fromPairs: fromPairs,
-    head: head,
-    indexOf: indexOf,
-    initial: initial,
-    intersection: intersection,
     parseInt: parseInt,
     repeat: repeat,
     unary: unary,
